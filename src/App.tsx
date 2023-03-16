62d8807124b79e989574a2be671e8ec046756440
chinyu.hsu <chinyu.hsu@imersive.com>
2023-03-16 10:08:13 +0800

    build: Initialize firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCsev_Qd-iCjwhCHYu3t4WAeX0eFJWgSgU",
+  authDomain: "shortening-url-6cf7a.firebaseapp.com",
+  projectId: "shortening-url-6cf7a",
+  storageBucket: "shortening-url-6cf7a.appspot.com",
+  messagingSenderId: "650938451288",
+  appId: "1:650938451288:web:0abc0c933e62eb1a89d12d",
+  measurementId: "G-HT90D8GWWM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
